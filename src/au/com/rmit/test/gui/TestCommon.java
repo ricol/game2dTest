a21704b0b97286e4752d8a51146764b9c3ff1c62
Ricol Wang <wangxinghe1983@gmail.com>
2015-08-08 18:10:21 +1000

    init

---
+++ src/au/com/rmit/test/gui/TestCommon.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package au.com.rmit.test.gui;
+
+/**
+ *
+ * @author Philology
+ */
+public class TestCommon
+{
+
+    public static int CATEGORY_WALL = 1;
+    public static int CATEGORY_CIRCLE = 2;
+}
---
+++ src/au/com/rmit/test/universe/Star.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package au.com.rmit.test.universe;
+
+/**
+ *
+ * @author Philology
+ */
+public class Star extends Entity
+{
+    public Star()
+    {
+        super();
+        
+        this.setRed(255);
+    }
+}
