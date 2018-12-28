8633e57fce4a2aa74484b538bba261f594f75643
Ricol Wang <ricol@opensimsim.com>
2018-12-28 14:56:02 +1100

    Menu

--- src/com/wang/test/gui/FrameMain.java
+++ src/com/wang/test/gui/FrameMain.java
@@ -1,6 +1,7 @@
 package com.wang.test.gui;
 
 import com.wang.Game2dEngine.director.Director;
+import com.wang.Game2dEngine.scene.Scene;
 import com.wang.common.JavaTheme;
 import com.wang.test.fireworks.FireworksScene;
 import com.wang.test.fountain.FountainScene;
@@ -44,17 +45,18 @@ public class FrameMain extends javax.swing.JFrame
     private void initComponents()
     {
 
-        jPanel4 = new javax.swing.JPanel();
-        jPanel3 = new javax.swing.JPanel();
-        btnClose = new javax.swing.JButton();
-        btnPause = new javax.swing.JButton();
-        jPanel2 = new javax.swing.JPanel();
-        btnFountain = new javax.swing.JButton();
-        btnFireworks = new javax.swing.JButton();
-        btnGravity = new javax.swing.JButton();
-        btnPinball = new javax.swing.JButton();
-        btnMath = new javax.swing.JButton();
         panelGame = new javax.swing.JPanel();
+        menuBar = new javax.swing.JMenuBar();
+        menuGame = new javax.swing.JMenu();
+        menuFountain = new javax.swing.JMenuItem();
+        menuFireworks = new javax.swing.JMenuItem();
+        menuMath = new javax.swing.JMenuItem();
+        menuPinball = new javax.swing.JMenuItem();
+        menuUniverse = new javax.swing.JMenuItem();
+        jSeparator1 = new javax.swing.JPopupMenu.Separator();
+        menuExit = new javax.swing.JMenuItem();
+        menuOption = new javax.swing.JMenu();
+        menuPause = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         addWindowListener(new java.awt.event.WindowAdapter()
@@ -64,82 +66,6 @@ public class FrameMain extends javax.swing.JFrame
                 formWindowOpened(evt);
             }
         });
-
-        btnClose.setText("Close");
-        btnClose.addActionListener(new java.awt.event.ActionListener()
-        {
-            public void actionPerformed(java.awt.event.ActionEvent evt)
-            {
-                btnCloseActionPerformed(evt);
-            }
-        });
-        jPanel3.add(btnClose);
-
-        btnPause.setText("Pause");
-        btnPause.addActionListener(new java.awt.event.ActionListener()
-        {
-            public void actionPerformed(java.awt.event.ActionEvent evt)
-            {
-                btnPauseActionPerformed(evt);
-            }
-        });
-        jPanel3.add(btnPause);
-
-        jPanel4.add(jPanel3);
-
-        btnFountain.setText("Fountain");
-        btnFountain.addActionListener(new java.awt.event.ActionListener()
-        {
-            public void actionPerformed(java.awt.event.ActionEvent evt)
-            {
-                btnFountainActionPerformed(evt);
-            }
-        });
-        jPanel2.add(btnFountain);
-
-        btnFireworks.setText("Fireworks");
-        btnFireworks.addActionListener(new java.awt.event.ActionListener()
-        {
-            public void actionPerformed(java.awt.event.ActionEvent evt)
-            {
-                btnFireworksActionPerformed(evt);
-            }
-        });
-        jPanel2.add(btnFireworks);
-
-        btnGravity.setText("Universe");
-        btnGravity.addActionListener(new java.awt.event.ActionListener()
-        {
-            public void actionPerformed(java.awt.event.ActionEvent evt)
-            {
-                btnGravityActionPerformed(evt);
-            }
-        });
-        jPanel2.add(btnGravity);
-
-        btnPinball.setText("Pinball");
-        btnPinball.addActionListener(new java.awt.event.ActionListener()
-        {
-            public void actionPerformed(java.awt.event.ActionEvent evt)
-            {
-                btnPinballActionPerformed(evt);
-            }
-        });
-        jPanel2.add(btnPinball);
-
-        btnMath.setText("Math");
-        btnMath.addActionListener(new java.awt.event.ActionListener()
-        {
-            public void actionPerformed(java.awt.event.ActionEvent evt)
-            {
-                btnMathActionPerformed(evt);
-            }
-        });
-        jPanel2.add(btnMath);
-
-        jPanel4.add(jPanel2);
-
-        getContentPane().add(jPanel4, java.awt.BorderLayout.PAGE_START);
 
         panelGame.setBackground(new java.awt.Color(0, 102, 102));
 
@@ -151,10 +77,91 @@ public class FrameMain extends javax.swing.JFrame
         );
         panelGameLayout.setVerticalGroup(
             panelGameLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 522, Short.MAX_VALUE)
+            .addGap(0, 550, Short.MAX_VALUE)
         );
 
         getContentPane().add(panelGame, java.awt.BorderLayout.CENTER);
+
+        menuGame.setText("Game");
+
+        menuFountain.setText("Foundtain");
+        menuFountain.addActionListener(new java.awt.event.ActionListener()
+        {
+            public void actionPerformed(java.awt.event.ActionEvent evt)
+            {
+                menuFountainActionPerformed(evt);
+            }
+        });
+        menuGame.add(menuFountain);
+
+        menuFireworks.setText("Fireworks");
+        menuFireworks.addActionListener(new java.awt.event.ActionListener()
+        {
+            public void actionPerformed(java.awt.event.ActionEvent evt)
+            {
+                menuFireworksActionPerformed(evt);
+            }
+        });
+        menuGame.add(menuFireworks);
+
+        menuMath.setText("Math");
+        menuMath.addActionListener(new java.awt.event.ActionListener()
+        {
+            public void actionPerformed(java.awt.event.ActionEvent evt)
+            {
+                menuMathActionPerformed(evt);
+            }
+        });
+        menuGame.add(menuMath);
+
+        menuPinball.setText("Pinball");
+        menuPinball.addActionListener(new java.awt.event.ActionListener()
+        {
+            public void actionPerformed(java.awt.event.ActionEvent evt)
+            {
+                menuPinballActionPerformed(evt);
+            }
+        });
+        menuGame.add(menuPinball);
+
+        menuUniverse.setText("Universe");
+        menuUniverse.addActionListener(new java.awt.event.ActionListener()
+        {
+            public void actionPerformed(java.awt.event.ActionEvent evt)
+            {
+                menuUniverseActionPerformed(evt);
+            }
+        });
+        menuGame.add(menuUniverse);
+        menuGame.add(jSeparator1);
+
+        menuExit.setText("Exit");
+        menuExit.addActionListener(new java.awt.event.ActionListener()
+        {
+            public void actionPerformed(java.awt.event.ActionEvent evt)
+            {
+                menuExitActionPerformed(evt);
+            }
+        });
+        menuGame.add(menuExit);
+
+        menuBar.add(menuGame);
+
+        menuOption.setText("Option");
+
+        menuPause.setText("Pause");
+        menuPause.addActionListener(new java.awt.event.ActionListener()
+        {
+            public void actionPerformed(java.awt.event.ActionEvent evt)
+            {
+                menuPauseActionPerformed(evt);
+            }
+        });
+        menuOption.add(menuPause);
+
+        menuBar.add(menuOption);
+
+        setJMenuBar(menuBar);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -164,63 +171,54 @@ public class FrameMain extends javax.swing.JFrame
         Director.getSharedInstance().setParent(this.panelGame);
     }//GEN-LAST:event_formWindowOpened
 
-    private void btnPauseActionPerformed(java.awt.event.ActionEvent evt)//GEN-FIRST:event_btnPauseActionPerformed
-    {//GEN-HEADEREND:event_btnPauseActionPerformed
-        if (Director.getSharedInstance().currentScene != null)
-        {
-            Director.getSharedInstance().currentScene.pause();
-        }
-
-        if (Director.getSharedInstance().currentScene.isScenePaused())
-        {
-            this.btnPause.setText("Continue");
-        } else
-        {
-            this.btnPause.setText("Pause");
-        }
-    }//GEN-LAST:event_btnPauseActionPerformed
-
-    private void btnCloseActionPerformed(java.awt.event.ActionEvent evt)//GEN-FIRST:event_btnCloseActionPerformed
-    {//GEN-HEADEREND:event_btnCloseActionPerformed
+    private void menuExitActionPerformed(java.awt.event.ActionEvent evt)//GEN-FIRST:event_menuExitActionPerformed
+    {//GEN-HEADEREND:event_menuExitActionPerformed
         exit(0);
-    }//GEN-LAST:event_btnCloseActionPerformed
+    }//GEN-LAST:event_menuExitActionPerformed
 
-    private void btnFountainActionPerformed(java.awt.event.ActionEvent evt)//GEN-FIRST:event_btnFountainActionPerformed
-    {//GEN-HEADEREND:event_btnFountainActionPerformed
+    private void menuFountainActionPerformed(java.awt.event.ActionEvent evt)//GEN-FIRST:event_menuFountainActionPerformed
+    {//GEN-HEADEREND:event_menuFountainActionPerformed
         Director.getSharedInstance().showScene(new FountainScene());
-    }//GEN-LAST:event_btnFountainActionPerformed
+    }//GEN-LAST:event_menuFountainActionPerformed
 
-    private void btnFireworksActionPerformed(java.awt.event.ActionEvent evt)//GEN-FIRST:event_btnFireworksActionPerformed
-    {//GEN-HEADEREND:event_btnFireworksActionPerformed
+    private void menuFireworksActionPerformed(java.awt.event.ActionEvent evt)//GEN-FIRST:event_menuFireworksActionPerformed
+    {//GEN-HEADEREND:event_menuFireworksActionPerformed
         Director.getSharedInstance().showScene(new FireworksScene());
-    }//GEN-LAST:event_btnFireworksActionPerformed
+    }//GEN-LAST:event_menuFireworksActionPerformed
 
-    private void btnGravityActionPerformed(java.awt.event.ActionEvent evt)//GEN-FIRST:event_btnGravityActionPerformed
-    {//GEN-HEADEREND:event_btnGravityActionPerformed
-        Director.getSharedInstance().showScene(new GravityDemoScene());
-    }//GEN-LAST:event_btnGravityActionPerformed
-
-    private void btnPinballActionPerformed(java.awt.event.ActionEvent evt)//GEN-FIRST:event_btnPinballActionPerformed
-    {//GEN-HEADEREND:event_btnPinballActionPerformed
-        Director.getSharedInstance().showScene(new PinballScene());
-    }//GEN-LAST:event_btnPinballActionPerformed
-
-    private void btnMathActionPerformed(java.awt.event.ActionEvent evt)//GEN-FIRST:event_btnMathActionPerformed
-    {//GEN-HEADEREND:event_btnMathActionPerformed
+    private void menuMathActionPerformed(java.awt.event.ActionEvent evt)//GEN-FIRST:event_menuMathActionPerformed
+    {//GEN-HEADEREND:event_menuMathActionPerformed
         Director.getSharedInstance().showScene(new MathScene());
-    }//GEN-LAST:event_btnMathActionPerformed
+    }//GEN-LAST:event_menuMathActionPerformed
+
+    private void menuPinballActionPerformed(java.awt.event.ActionEvent evt)//GEN-FIRST:event_menuPinballActionPerformed
+    {//GEN-HEADEREND:event_menuPinballActionPerformed
+        Director.getSharedInstance().showScene(new PinballScene());
+    }//GEN-LAST:event_menuPinballActionPerformed
+
+    private void menuUniverseActionPerformed(java.awt.event.ActionEvent evt)//GEN-FIRST:event_menuUniverseActionPerformed
+    {//GEN-HEADEREND:event_menuUniverseActionPerformed
+        Director.getSharedInstance().showScene(new GravityDemoScene());
+    }//GEN-LAST:event_menuUniverseActionPerformed
+
+    private void menuPauseActionPerformed(java.awt.event.ActionEvent evt)//GEN-FIRST:event_menuPauseActionPerformed
+    {//GEN-HEADEREND:event_menuPauseActionPerformed
+        Scene theScene = Director.getSharedInstance().currentScene;
+        if (theScene != null) theScene.pause();
+    }//GEN-LAST:event_menuPauseActionPerformed
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btnClose;
-    private javax.swing.JButton btnFireworks;
-    private javax.swing.JButton btnFountain;
-    private javax.swing.JButton btnGravity;
-    private javax.swing.JButton btnMath;
-    private javax.swing.JButton btnPause;
-    private javax.swing.JButton btnPinball;
-    private javax.swing.JPanel jPanel2;
-    private javax.swing.JPanel jPanel3;
-    private javax.swing.JPanel jPanel4;
+    private javax.swing.JPopupMenu.Separator jSeparator1;
+    private javax.swing.JMenuBar menuBar;
+    private javax.swing.JMenuItem menuExit;
+    private javax.swing.JMenuItem menuFireworks;
+    private javax.swing.JMenuItem menuFountain;
+    private javax.swing.JMenu menuGame;
+    private javax.swing.JMenuItem menuMath;
+    private javax.swing.JMenu menuOption;
+    private javax.swing.JMenuItem menuPause;
+    private javax.swing.JMenuItem menuPinball;
+    private javax.swing.JMenuItem menuUniverse;
     private javax.swing.JPanel panelGame;
     // End of variables declaration//GEN-END:variables
 }
